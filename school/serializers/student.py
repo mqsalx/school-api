0efa0a8b0bd87270a5ddd575d82e135faaf18c62
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-09-06 14:19:14 -0300

    updated
    Validation Improvement.

--- school/serializers/student.py
+++ school/serializers/student.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from school.models.student import StudentModel
+from school.serializers.validators import cpf_validate, name_alpha, phone_len
 
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -14,3 +15,22 @@ class StudentSerializer(serializers.ModelSerializer):
             "b_day",
             "phone",
         ]
+
+    def validate(self, data):
+
+        if name_alpha(data["name"]):
+            raise serializers.ValidationError(
+                {"name": "Name must have only letters!"}
+            )
+
+        if cpf_validate(data["cpf"]):
+            raise serializers.ValidationError(
+                {"cpf": "CPF must have a valid value!"}
+            )
+
+        if phone_len(data["phone"]):
+            raise serializers.ValidationError(
+                {"phone": "Phone must have 11 digits! 99 99999-9999"}
+            )
+
+        return data
---
+++ school/serializers/validators.py
@@ -0,0 +1,20 @@
+import re
+
+from validate_docbr import CPF
+
+
+def name_alpha(name_value):
+    return not name_value.isalpha()
+
+
+def cpf_validate(cpf_value):
+    cpf = CPF()
+    cpf_is_valid = cpf.validate(cpf_value)
+    return not cpf_is_valid
+
+
+def phone_len(phone_value):
+    # 86 99999-9999
+    model = "[0-9]{2} [0-9]{5}-[0-9]{4}"
+    response = re.findall(model, phone_value)
+    return not response
