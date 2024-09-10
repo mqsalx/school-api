488a4cfb69a1f28910f2ddc457064026142bc809
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-09-10 10:51:51 -0300

    updated
    Configured Throttling specifically for the registration part.

--- school/views/registration.py
+++ school/views/registration.py
@@ -1,11 +1,20 @@
-
 from rest_framework.viewsets import ModelViewSet
 
 from school.models.registration import RegistrationModel
 from school.serializers.registration import RegistrationSerializer
+from school.views.throttles.anon_rate_throttle import (
+    RegistrationAnonRateThrottle,
+)
+from school.views.throttles.user_rate_throttle import (
+    RegistrationUserRateThrottle,
+)
 
 
 class RegistrationView(ModelViewSet):
 
     queryset = RegistrationModel.objects.all().order_by("id")
     serializer_class = RegistrationSerializer
+    throttle_classes = [
+        RegistrationUserRateThrottle,
+        RegistrationAnonRateThrottle,
+    ]
