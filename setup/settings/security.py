f325443baff678d0b70b66f4ae6b5ca68e6caed0
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-09-10 10:30:02 -0300

    updated
    Configured Throttling globally for API restrictions.

--- setup/settings/security.py
+++ setup/settings/security.py
@@ -23,4 +23,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
     "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.QueryParameterVersioning",
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "50/day", "user": "20/day"},
 }
