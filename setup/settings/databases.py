60b71e72bc865ec2323f3ea3769903ab268df2ae
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-09-27 19:12:45 -0300

    updated
    Refactoring to better place to get environment variables.

--- setup/settings/databases.py
+++ setup/settings/databases.py
@@ -1,28 +1,26 @@
+# setup\settings\databases.py
+
 import os
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("DATABASE_ENGINE"),
-        "NAME": (
-            os.environ.get("DATABASE_NAME_DEV")
-            if os.environ.get("ENV_MODE") == "development"
-            else os.environ.get("DATABASE_NAME_PROD")
-        ),
-        "USER": (
-            os.environ.get("DATABASE_USER_DEV")
-            if os.environ.get("ENV_MODE") == "development"
-            else os.environ.get("DATABASE_USER_PROD")
-        ),
-        "PASSWORD": (
-            os.environ.get("DATABASE_PASSWORD_DEV")
-            if os.environ.get("ENV_MODE") == "development"
-            else os.environ.get("DATABASE_PASSWORD_PROD")
-        ),
-        "HOST": os.environ.get("DATABASE_HOST"),
-        "PORT": os.environ.get("DATABASE_PORT"),
+if os.environ.get("ENV_MODE") == "development":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": os.environ.get("DATABASE_ENGINE"),
+            "NAME": os.environ.get("DATABASE_NAME"),
+            "USER": os.environ.get("DATABASE_USER"),
+            "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+            "HOST": os.environ.get("DATABASE_HOST"),
+            "PORT": os.environ.get("DATABASE_PORT"),
+        }
+    }
