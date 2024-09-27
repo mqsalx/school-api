9d2f68ea7faa09fe2103e1bdd5ad2f8ec5ff1da5
Alex Marques <66804324+mqsalx@users.noreply.github.com>
2024-09-27 19:03:38 -0300

    refactor
    Added parameters for JWT operation.

--- school/urls/setup.py
+++ school/urls/setup.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from school.views.course import CourseView
 from school.views.documentation import schema_view
@@ -39,4 +44,11 @@ urlpatterns = [
         schema_view.with_ui("redoc", cache_timeout=0),
         name="schema-redoc",
     ),
+    path(
+        "api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"
+    ),
+    path(
+        "api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"
+    ),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
